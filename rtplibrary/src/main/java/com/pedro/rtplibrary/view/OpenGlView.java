4c7882348277203b64ef5713224dd75b1cb56a63
CubitSpeed <67028273+CubitSpeed@users.noreply.github.com>
2020-07-26 15:07:18 -0700

    working but flow framerate on 1st draw

--- rtplibrary/src/main/java/com/pedro/rtplibrary/view/OpenGlView.java
+++ rtplibrary/src/main/java/com/pedro/rtplibrary/view/OpenGlView.java
@@ -134,6 +134,21 @@ public class OpenGlView extends OpenGlViewBase {
               true, false, false);
           surfaceManager.swapBuffer();
 
+
+          synchronized (sync2) {
+            if (surfaceManagerEncoder2 != null && !fpsLimiter.limitFPS()) {
+              surfaceManagerEncoder2.makeCurrent();
+              if (muteVideo) {
+                managerRender.drawScreen(0, 0, false, aspectRatioMode, streamRotation, false,
+                        isStreamVerticalFlip, isStreamHorizontalFlip);
+              } else {
+                managerRender.drawScreen(1920, 1080, false, aspectRatioMode,
+                        streamRotation, false, isStreamVerticalFlip, isStreamHorizontalFlip);
+              }
+              surfaceManagerEncoder2.swapBuffer();
+            }
+          }
+
           synchronized (sync) {
             if (surfaceManagerEncoder != null && !fpsLimiter.limitFPS()) {
               surfaceManagerEncoder.makeCurrent();
@@ -156,6 +171,8 @@ public class OpenGlView extends OpenGlViewBase {
             if (surfaceManagerEncoder != null) surfaceManagerEncoder.swapBuffer();
             else if (surfaceManagerPhoto != null) surfaceManagerPhoto.swapBuffer();
           }
+
+
           if (!filterQueue.isEmpty()) {
             Filter filter = filterQueue.take();
             managerRender.setFilter(filter.getPosition(), filter.getBaseFilterRender());
