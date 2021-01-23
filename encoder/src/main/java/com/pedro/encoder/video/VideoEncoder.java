109973b5519b0531df9b6d7985643fa3164095d9
CubitSpeed <67028273+CubitSpeed@users.noreply.github.com>
2021-01-23 00:01:27 -0800

    actually limit fps on the fly

--- encoder/src/main/java/com/pedro/encoder/video/VideoEncoder.java
+++ encoder/src/main/java/com/pedro/encoder/video/VideoEncoder.java
@@ -249,6 +249,7 @@ public class VideoEncoder extends BaseEncoder implements GetCameraData {
 
   public void setFps(int fps) {
     this.fps = fps;
+    fpsLimiter.setFPS(fps);
   }
 
   public int getFps() {
