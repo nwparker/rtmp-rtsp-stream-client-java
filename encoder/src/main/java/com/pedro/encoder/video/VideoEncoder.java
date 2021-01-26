9eff5da8688ec2cccbfe6335e5ea3d39903f950e
CubitSpeed <67028273+CubitSpeed@users.noreply.github.com>
2021-01-25 23:46:30 -0800

    release surface on exception

--- encoder/src/main/java/com/pedro/encoder/video/VideoEncoder.java
+++ encoder/src/main/java/com/pedro/encoder/video/VideoEncoder.java
@@ -148,6 +148,9 @@ public class VideoEncoder extends BaseEncoder implements GetCameraData {
       return true;
     } catch (IOException | IllegalStateException e) {
       Log.e(TAG, "Create VideoEncoder failed.", e);
+      if (inputSurface != null) {
+        inputSurface.release();
+      }
       return false;
     }
   }
